4d0203effb59b34738d28ccf936484696de29b3b
Yair Ponce <yair.ponce@it-and-apps.mx>
2022-11-09 19:09:17 -0600

    clase 16

--- msvc-usuarios/src/main/java/org/jmarquez/springcloud/msvc/usuarios/controllers/UsuarioController.java
+++ msvc-usuarios/src/main/java/org/jmarquez/springcloud/msvc/usuarios/controllers/UsuarioController.java
@@ -49,6 +49,14 @@ public class UsuarioController {
         return ResponseEntity.notFound().build();
     }
 
-
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> borrar(@PathVariable Long id){
+        Optional<Usuario> o = service.porId(id);
+        if(o.isPresent()){
+            service.eliminar(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 
 }
